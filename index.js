f0a35f8412c766a71c85e8bd77000936b6ce757f
Jack Cannon <jack.cannon@holidayextras.com>
2013-04-15 17:17:50 +0100

    Fixed index.js

--- index.js
+++ index.js
@@ -19,52 +19,50 @@ function jRun (options, cb) {
 jRun.prototype.run = function() {
   var self = this;
 
+  self.getFiles(function (err, files) {
+    self.emit('start', files);
+    async.map(files, function(item, callback){
+      var runTest = spawn('node', [ self._options.dirname + '/' + item ]);
+      var storedData = [];
+
+      runTest.stdout.setEncoding('utf8');
+      runTest.stdout.on('data', function (data) {
+        storedData.push({type: 'stdout', data: data});
+      });
+
+      runTest.stderr.setEncoding('utf8');
+      runTest.stderr.on('data', function (data) {
+        storedData.push({type: 'stderr', data: data});
+      });
+      runTest.on('exit', function (code) {
+        callback(null, { code: code, data: storedData });
+        self.emit('fileComplete', code, item, storedData);
+      });
+    },
+    function(err, results){
+      self._cb(err, results);
+      self.emit('complete');
+
+      if(results.code) {
+        process.exit(1);
+      } else {
+        process.exit(0);
+      }
+    });
+  })
+}
+
+jRun.prototype.getFiles = function (cb) {
+  var self = this;
   fs.readdir(self._options.dirname, function (err, files) {
     if(err) return self._cb('Not a valid dir');
     
     async.filter(files, function (item, callback) {
       callback(self._options.filter.test(item));
     }, function (filteredFiles) {
-      
-      self.emit('start', filteredFiles);
-
-      async.each(filteredFiles, function(item, callback){
-        var runTest = spawn('node', [ self._options.dirname + '/' + item ]);
-        var storedData = [];
-
-        runTest.stdout.setEncoding('utf8');
-        runTest.stdout.on('data', function (data) {
-          storedData.push({type: 'stdout', data: data});
-        });
-
-        runTest.stderr.setEncoding('utf8');
-        runTest.stderr.on('data', function (data) {
-          storedData.push({type: 'stderr', data: data});
-        });
-        runTest.on('exit', function (code) {
-          callback(null, { code: code, data: storedData });
-          self.emit('fileComplete', code, item, storedData);
-        });
-      },
-      function(err, results){
-        self._cb(err, results);
-        self.emit('complete');
-      });
+      cb(null, filteredFiles);
     });
   });
-
-  // async.each(Object.keys(tests), function(item, callback){
-  //   var test = new tests[item]();
-  //   test.runTests(callback);
-  // },
-  // function(err, results){
-  //   cb(err, results);
-  //   if(err) {
-  //     process.exit(1);
-  //   } else {
-  //     process.exit(0);
-  //   }
-  // });
 }
 
 
