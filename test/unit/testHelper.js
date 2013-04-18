9e84e4787824b2298dccdc520c2be2be9410576a
joewarren <joe.warren@holidayextras.com>
2013-04-18 09:15:22 +0100

    Fix tests

--- test/unit/testHelper.js
+++ test/unit/testHelper.js
@@ -1,11 +1,16 @@
 var helper = require('../../lib/helper');
 var assert = require('assert');
+var _ = require('underscore');
 
 (function(){
   helper.walk('test/files', function(err, files){
-    var testFiles = [ 'test/files/dontRunMe.js',
-  'test/files/file.js',
-  'test/files/runMe.js' ]
-    assert.deepEqual(files, testFiles);
+    var testFiles = [
+      'test/files/dontRunMe.js',
+      'test/files/file.js',
+      'test/files/runMe.js'
+    ];
+
+    var difference = _.difference(testFiles, files);
+    assert.deepEqual(difference, []);
   })
 })();
