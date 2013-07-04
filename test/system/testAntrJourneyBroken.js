8c2064e3ba89ec21b49ded6ed6d957a73fdfa4b2
joewarren <joe.warren@holidayextras.com>
2013-07-04 12:57:34 +0100

    Removed uncessary code

--- test/system/testAntrJourneyBroken.js
+++ test/system/testAntrJourneyBroken.js
@@ -1,8 +1,5 @@
 var Antr = require('../../');
 var assert = require('assert');
-var _ = require('underscore');
-
-var path = __dirname + '/../dummyTests';
 
 var run = new Antr({
   filter: /(dummy.+)\.js/,
